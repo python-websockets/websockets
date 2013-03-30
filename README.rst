392b5464e41fdef203c517cd387c997b685ddb5a
Aymeric Augustin <aymeric.augustin@m4x.org>
2013-03-30 23:02:16 +0100

    Initialize repository.

---
+++ README.rst
@@ -0,0 +1,12 @@
+README
+======
+
+``websockets`` is an implementation of `RFC 6455`_ focused on simplicity and
+correctness.
+
+It relies on the `Tulip`_ library, which prefigurates Python's `asynchronous
+IO support`_, and requires Python ≥ 3.3.
+
+.. _RFC 6455: http://tools.ietf.org/html/rfc6455
+.. _Tulip: http://code.google.com/p/tulip/
+.. _asynchronous IO support: http://www.python.org/dev/peps/pep-3156/
---
+++ setup.py
@@ -0,0 +1,36 @@
+import distutils.core
+import os
+
+# Avoid polluting the .tar.gz with ._* files under Mac OS X
+os.putenv('COPYFILE_DISABLE', 'true')
+
+description = "An implementation of the WebSocket Protocol (RFC 6455)"
+
+with open(os.path.join(os.path.dirname(__file__), 'README')) as f:
+    long_description = '\n\n'.join(f.read().split('\n\n')[2:4])
+
+distutils.core.setup(
+    name='websockets',
+    version='0.1',
+    author='Aymeric Augustin',
+    author_email='aymeric.augustin@m4x.org',
+    url='https://github.com/aaugustin/websockets',
+    description=description,
+    long_description=long_description,
+    download_url='https://pypi.python.org/pypi/websockets',
+    packages=[
+        'websockets',
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+    ],
+    platforms='all',
+    license='BSD'
+)

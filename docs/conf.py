ac77aab2cdad9046982b16802293796c031b81d3
Aymeric Augustin <aymeric.augustin@m4x.org>
2013-04-01 11:53:10 +0200

    Use Python 3's theme for the documentation.

--- docs/conf.py
+++ docs/conf.py
@@ -91,7 +91,8 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'pydoctheme'
+html_theme_path = ['.']
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -243,4 +244,4 @@ texinfo_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'http://docs.python.org/': None}
+intersphinx_mapping = {'http://docs.python.org/3/': None}

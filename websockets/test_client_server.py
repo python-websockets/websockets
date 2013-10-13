af6ba46e680259fe1464b8b463b02bea7e83e9e4
Aymeric Augustin <aymeric.augustin@m4x.org>
2013-10-13 11:47:51 +0200

    Simplify tests with unittest.mock.patch.

--- websockets/test_client_server.py
+++ websockets/test_client_server.py
@@ -8,10 +8,9 @@ from unittest.mock import patch
 
 import tulip
 
-from . import client
 from .client import *
 from .exceptions import InvalidHandshake
-from . import server
+from .http import read_response
 from .server import *
 
 
@@ -53,69 +52,48 @@ class ClientServerTests(unittest.TestCase):
         self.assertEqual(reply, "Hello!")
         self.stop_client()
 
-    def test_server_receives_malformed_request(self):
-        old_read_request = server.read_request
+    @patch('websockets.server.read_request')
+    def test_server_receives_malformed_request(self, _read_request):
+        _read_request.side_effect = ValueError("read_request failed")
+
+        with self.assertRaises(InvalidHandshake):
+            self.start_client()
+
+    @patch('websockets.client.read_response')
+    def test_client_receives_malformed_response(self, _read_response):
+        _read_response.side_effect = ValueError("read_response failed")
+
+        with self.assertRaises(InvalidHandshake):
+            self.start_client()
+
+    @patch('websockets.client.build_request')
+    def test_client_sends_invalid_handshake_request(self, _build_request):
+        def wrong_build_request(set_header):
+            return '42'
+        _build_request.side_effect = wrong_build_request
+
+        with self.assertRaises(InvalidHandshake):
+            self.start_client()
+
+    @patch('websockets.server.build_response')
+    def test_server_sends_invalid_handshake_response(self, _build_response):
+        def wrong_build_response(set_header, key):
+            return build_response(set_header, '42')
+        _build_response.side_effect = wrong_build_response
+
+        with self.assertRaises(InvalidHandshake):
+            self.start_client()
+
+    @patch('websockets.client.read_response')
+    def test_server_does_not_switch_protocols(self, _read_response):
         @tulip.coroutine
-        def read_request(stream):
-            yield from old_read_request(stream)
-            raise ValueError("Not sure what went wrong")
-        server.read_request = read_request
-        server.logger.setLevel(logging.ERROR)
-        try:
-            with self.assertRaises(InvalidHandshake):
-                self.start_client()
-        finally:
-            server.read_request = old_read_request
-            server.logger.setLevel(logging.NOTSET)
-
-    def test_client_receives_malformed_response(self):
-        old_read_response = client.read_response
-        @tulip.coroutine
-        def read_response(stream):
-            yield from old_read_response(stream)
-            raise ValueError("Not sure what went wrong")
-        client.read_response = read_response
-        try:
-            with self.assertRaises(InvalidHandshake):
-                self.start_client()
-        finally:
-            client.read_response = old_read_response
-
-    def test_client_sends_invalid_handshake_request(self):
-        old_build_request = client.build_request
-        def build_request(set_header):
-            old_build_request(set_header)
-            return '42'                                     # Use a wrong key.
-        client.build_request = build_request
-        try:
-            with self.assertRaises(InvalidHandshake):
-                self.start_client()
-        finally:
-            client.build_request = old_build_request
-
-    def test_server_sends_invalid_handshake_response(self):
-        old_build_response = server.build_response
-        def build_response(set_header, key):
-            old_build_response(set_header, '42')            # Use a wrong key.
-        server.build_response = build_response
-        try:
-            with self.assertRaises(InvalidHandshake):
-                self.start_client()
-        finally:
-            server.build_response = old_build_response
-
-    def test_server_does_not_switch_protocols(self):
-        old_read_response = client.read_response
-        @tulip.coroutine
-        def read_response(stream):
-            code, headers = yield from old_read_response(stream)
+        def wrong_read_response(stream):
+            code, headers = yield from read_response(stream)
             return 400, headers
-        client.read_response = read_response
-        try:
-            with self.assertRaises(InvalidHandshake):
-                self.start_client()
-        finally:
-            client.read_response = old_read_response
+        _read_response.side_effect = wrong_read_response
+
+        with self.assertRaises(InvalidHandshake):
+            self.start_client()
 
     @patch('websockets.server.WebSocketServerProtocol.send')
     def test_server_handler_crashes(self, send):

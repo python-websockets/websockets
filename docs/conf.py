bb64dc7aef10d55eebd139255263d35c7a092e76
Aymeric Augustin <aymeric.augustin@m4x.org>
2014-04-26 15:46:40 +0200

    Death to unicode literals.

--- docs/conf.py
+++ docs/conf.py
@@ -40,8 +40,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'websockets'
-copyright = u'2013-2014, Aymeric Augustin'
+project = 'websockets'
+copyright = '2013-2014, Aymeric Augustin'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -184,8 +184,8 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'websockets.tex', u'websockets Documentation',
-   u'Aymeric Augustin', 'manual'),
+  ('index', 'websockets.tex', 'websockets Documentation',
+   'Aymeric Augustin', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -214,8 +214,8 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'websockets', u'websockets Documentation',
-     [u'Aymeric Augustin'], 1)
+    ('index', 'websockets', 'websockets Documentation',
+     ['Aymeric Augustin'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -228,8 +228,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'websockets', u'websockets Documentation',
-   u'Aymeric Augustin', 'websockets', 'One line description of project.',
+  ('index', 'websockets', 'websockets Documentation',
+   'Aymeric Augustin', 'websockets', 'One line description of project.',
    'Miscellaneous'),
 ]
 

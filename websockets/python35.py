d9b845e0daa9a14e8a3418f71270d3d2f3c881e2
Szieberth Ádám <sziebadam@gmail.com>
2015-12-16 01:19:01 +0100

    __aenter__ moved down to a better location

--- websockets/python35.py
+++ websockets/python35.py
@@ -10,16 +10,16 @@ class _Connect:
         self.args = args
         self.kwargs = kwargs
 
-    async def __aenter__(self):
-        self._websocket = await self
-        return self._websocket
-
     def __iter__(self):
         cls = self.__class__
         coro = cls._client_connect_coro(*self.args, **self.kwargs)
         return (yield from coro)
 
     __await__ = __iter__
+
+    async def __aenter__(self):
+        self._websocket = await self
+        return self._websocket
 
     async def __aexit__(self, exc_type, exc_value, traceback):
         close_result = await self._websocket.close()

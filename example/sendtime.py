cffcf6d268fbacc93b85304e6c4d65458647c33f
cuiwei13 <cuiwei13@pku.edu.cn>
2016-06-05 22:32:03 +0800

    optimize sendtime example using sharing port feature.

--- example/sendtime.py
+++ example/sendtime.py
@@ -11,7 +11,26 @@ async def time(websocket, path):
         await websocket.send(now)
         await asyncio.sleep(random.random() * 3)
 
-start_server = websockets.serve(time, '127.0.0.1', 5678)
+class HTTPWebSocketServerProtocol(websockets.WebSocketServerProtocol):
+	
+	def hook_for_http_response(self, path, headers):
+		try:
+			with open('showtime.html', 'rb') as fp:
+				response = 'HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n'
+				self.writer.write(response.encode())
+				while True:
+					data = fp.read(4096)
+					if not data:
+						break
+					self.writer.write(data)
+		except Exception as e:
+			print(e)
+			self.writer.write('HTTP/1.0 404 Not Found\r\n\r\nFile not found'.encode())
+		finally:
+			self.writer.close()
+
+start_server = websockets.serve(time, '127.0.0.1', 5678, klass=HTTPWebSocketServerProtocol)
+print("Hi, please visit -> http://127.0.0.1:5678")
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()

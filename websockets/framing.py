2cf56e9af53d8a22ca320d817c657e9bc83dd27d
Aymeric Augustin <aymeric.augustin@m4x.org>
2017-05-09 15:58:07 +0200

    Refactor processing of frames.

--- websockets/framing.py
+++ websockets/framing.py
@@ -25,6 +25,7 @@ except ImportError:                                         # pragma: no cover
 
 
 __all__ = [
+    'DATA_OPCODES', 'CTRL_OPCODES',
     'OP_CONT', 'OP_TEXT', 'OP_BINARY', 'OP_CLOSE', 'OP_PING', 'OP_PONG',
     'Frame', 'parse_close', 'serialize_close'
 ]
@@ -89,8 +90,9 @@ class Frame(FrameData):
         If ``max_size`` is set and the payload exceeds this size in bytes,
         :exc:`~websockets.exceptions.PayloadTooBig` is raised.
 
-        If ``extensions`` is provided, it's a list of functions that transform
-        the frame and return it. They are applied in reverse order.
+        If ``extensions`` is provided, it's a list of classes with an
+        ``decode()`` method that transform the frame and return a new frame.
+        They are applied in order.
 
         This function validates the frame before returning it and raises
         :exc:`~websockets.exceptions.WebSocketProtocolError` if it contains
@@ -134,7 +136,7 @@ class Frame(FrameData):
         frame.check()
 
         for extension in reversed(extensions):
-            frame = extension(frame)
+            frame = extension.decode(frame)
 
         return frame
 
@@ -149,8 +151,9 @@ class Frame(FrameData):
         ``mask`` is a :class:`bool` telling whether the frame should be masked
         i.e. whether the write happens on the client side.
 
-        If ``extensions`` is provided, it's a list of functions that transform
-        the frame and return it. They are applied in order.
+        If ``extensions`` is provided, it's a list of classes with an
+        ``encode()`` method that transform the frame and return a new frame.
+        They are applied in order.
 
         This function validates the frame before sending it and raises
         :exc:`~websockets.exceptions.WebSocketProtocolError` if it contains
@@ -162,7 +165,7 @@ class Frame(FrameData):
         # but it's the instance of class to which this method is bound.
 
         for extension in extensions:
-            frame = extension(frame)
+            frame = extension.encode(frame)
 
         frame.check()
 

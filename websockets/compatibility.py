9f566c5d12eae1258aa6f1991fe8cf0a7d390f97
MysterialPy <mysterialpy@gmail.com>
2017-12-14 23:41:48 +1000

    Fix Syntax error thrown in 3.7
    Maintains backward compat.

--- websockets/compatibility.py
+++ websockets/compatibility.py
@@ -9,10 +9,10 @@ import http
 
 
 # Replace with BaseEventLoop.create_task when dropping Python < 3.4.2.
-try:                                                # pragma: no cover
-    asyncio_ensure_future = asyncio.ensure_future   # Python ≥ 3.5
-except AttributeError:                              # pragma: no cover
-    asyncio_ensure_future = asyncio.async           # Python < 3.5
+try:                                                     # pragma: no cover
+    asyncio_ensure_future = asyncio.ensure_future        # Python ≥ 3.5
+except AttributeError:                                   # pragma: no cover
+    asyncio_ensure_future = getattr(asyncio, 'async')    # Python < 3.5
 
 try:                                                # pragma: no cover
                                                     # Python ≥ 3.5

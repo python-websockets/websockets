4eee42b460a4f450fa16f5bac4cf68e0d77b25c5
Tobin Yehle <tobinyehle@gmail.com>
2019-04-02 16:42:25 -0600

    mark package as typed

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setuptools.setup(
     packages=packages,
     ext_modules=ext_modules,
     include_package_data=True,
-    zip_safe=True,
+    zip_safe=False,
     python_requires='>=3.6',
     test_loader='unittest:TestLoader',
 )

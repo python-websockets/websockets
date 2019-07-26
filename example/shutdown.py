35ee6d0a8b4f866a8b0c0fdabc15ebcda5ce205f
Hakan Karaoguz <hkaraoguz@yahoo.com.tr>
2019-07-26 16:14:29 +0200

    Update shutdown.py
    Catching KeyboardInterrupt exception instead of SIGINT

--- example/shutdown.py
+++ example/shutdown.py
@@ -14,12 +14,13 @@ async def echo_server(stop):
 
 loop = asyncio.get_event_loop()
 
-# The stop condition is set when receiving SIGTERM.
-stop = loop.create_future()
-loop.add_signal_handler(signal.SIGTERM, stop.set_result, None)
+try:
+    # The stop condition is set when receiving SIGTERM.
+    stop = loop.create_future()
+    loop.add_signal_handler(signal.SIGTERM, stop.set_result, None)
+    
+    # Run the server until the stop condition is met.
+    loop.run_until_complete(echo_server(stop))
 
-# For CTRL+C signal
-loop.add_signal_handler(signal.SIGINT, stop.set_result, None)
-
-# Run the server until the stop condition is met.
-loop.run_until_complete(echo_server(stop))
+except KeyboardInterrupt:
+    print("Process interrupted")

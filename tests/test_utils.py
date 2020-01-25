cf5af352200e6800f0152c8399af067a45053d76
Aymeric Augustin <aymeric.augustin@m4x.org>
2020-01-25 21:32:21 +0100

    Extract generate_key() from the legacy handshake module.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,8 @@
+import base64
 import itertools
 import unittest
 
-from websockets.utils import accept_key, apply_mask as py_apply_mask
+from websockets.utils import accept_key, apply_mask as py_apply_mask, generate_key
 
 
 # Test vector from RFC 6455
@@ -9,7 +10,11 @@ KEY = "dGhlIHNhbXBsZSBub25jZQ=="
 ACCEPT = "s3pPLMBiTxaQ9kYGzzhZRbK+xOo="
 
 
-class AcceptKeyTests(unittest.TestCase):
+class UtilsTests(unittest.TestCase):
+    def test_generate_key(self):
+        key = generate_key()
+        self.assertEqual(len(base64.b64decode(key.encode())), 16)
+
     def test_accept_key(self):
         self.assertEqual(accept_key(KEY), ACCEPT)
 

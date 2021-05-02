56be5f71e273fee7a2ef86166838f574b58e3c59
Aymeric Augustin <aymeric.augustin@m4x.org>
2021-05-02 15:36:56 +0200

    Build wheels on Python 3.9.

--- setup.py
+++ setup.py
@@ -54,6 +54,7 @@ setuptools.setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     package_dir = {'': 'src'},
     package_data = {'websockets': ['py.typed']},

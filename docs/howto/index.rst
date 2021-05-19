af5bfd1b52b70ace251abe2e0ca7541b316ea2c3
Aymeric Augustin <aymeric.augustin@m4x.org>
2021-05-19 09:09:58 +0200

    Deployment deployment under Supervisor.
    Refs #445.

--- docs/howto/index.rst
+++ docs/howto/index.rst
@@ -34,3 +34,4 @@ optimized, and secure setup.
    compression
    heroku
    kubernetes
+   supervisor
---
+++ example/deployment/supervisor/app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import asyncio
+import signal
+
+import websockets
+
+
+async def echo(websocket, path):
+    async for message in websocket:
+        await websocket.send(message)
+
+
+async def main():
+    # Set the stop condition when receiving SIGTERM.
+    loop = asyncio.get_running_loop()
+    stop = loop.create_future()
+    loop.add_signal_handler(signal.SIGTERM, stop.set_result, None)
+
+    async with websockets.serve(
+        echo, "", 8080,
+        reuse_port=True,
+    ):
+        await stop
+
+
+if __name__ == "__main__":
+    asyncio.run(main())

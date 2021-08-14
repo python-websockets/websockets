cc35695df01241752d01266ad3d86510505af070
Aymeric Augustin <aymeric.augustin@m4x.org>
2021-08-14 09:10:48 +0200

    Reformat conf.py with black.

--- docs/conf.py
+++ docs/conf.py
@@ -13,18 +13,17 @@ import sys
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-
-sys.path.insert(0, os.path.join(os.path.abspath('..'), 'src'))
+sys.path.insert(0, os.path.join(os.path.abspath(".."), "src"))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'websockets'
-copyright = f'2013-{datetime.date.today().year}, Aymeric Augustin and contributors'
-author = 'Aymeric Augustin'
+project = "websockets"
+copyright = f"2013-{datetime.date.today().year}, Aymeric Augustin and contributors"
+author = "Aymeric Augustin"
 
 # The full version, including alpha/beta/rc tags
-release = '9.1'
+release = "9.1"
 
 
 # -- General configuration ---------------------------------------------------
@@ -33,53 +32,52 @@ release = '9.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.viewcode',
-    'sphinx_autodoc_typehints',
-    'sphinxcontrib.spelling',
-    'sphinxcontrib_trio',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.viewcode",
+    "sphinx_autodoc_typehints",
+    "sphinxcontrib.spelling",
+    "sphinxcontrib_trio",
 ]
 
-intersphinx_mapping = {'python': ('https://docs.python.org/3', None)}
+intersphinx_mapping = {"python": ("https://docs.python.org/3", None)}
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
-html_theme = 'alabaster'
+html_theme = "alabaster"
 
 html_theme_options = {
-    'logo': 'websockets.svg',
-    'description': 'A library for building WebSocket servers and clients in Python with a focus on correctness and simplicity.',
-    'github_button': True,
-    'github_type': 'star',
-    'github_user': 'aaugustin',
-    'github_repo': 'websockets',
-    'tidelift_url': 'https://tidelift.com/subscription/pkg/pypi-websockets?utm_source=pypi-websockets&utm_medium=referral&utm_campaign=docs',
+    "logo": "websockets.svg",
+    "description": "A library for building WebSocket servers and clients in Python with a focus on correctness and simplicity.",
+    "github_button": True,
+    "github_type": "star",
+    "github_user": "aaugustin",
+    "github_repo": "websockets",
+    "tidelift_url": "https://tidelift.com/subscription/pkg/pypi-websockets?utm_source=pypi-websockets&utm_medium=referral&utm_campaign=docs",
 }
 
 html_sidebars = {
-    '**': [
-        'about.html',
-        'searchbox.html',
-        'navigation.html',
-        'relations.html',
-        'donate.html',
+    "**": [
+        "about.html",
+        "searchbox.html",
+        "navigation.html",
+        "relations.html",
+        "donate.html",
     ]
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]

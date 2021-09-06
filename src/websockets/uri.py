17f06cc0c661887ea423eab2d730315c053e879c
Aymeric Augustin <aymeric.augustin@m4x.org>
2021-09-06 21:45:48 +0200

    Refactor WebSocketURI.

--- src/websockets/uri.py
+++ src/websockets/uri.py
@@ -17,11 +17,12 @@ class WebSocketURI:
 
     Attributes:
         secure: :obj:`True` for a ``wss`` URI, :obj:`False` for a ``ws`` URI.
-        host: Host, normalized to lower case.
-        port: Port, always set even if it's the default.
-        resource_name: Path and optional query.
-        user_info: ``(username, password)`` when the URI contains
-            `User Information`_, else :obj:`None`.
+        host: Normalized to lower case.
+        port: Always set even if it's the default.
+        path: May be empty.
+        query: May be empty if the URI doesn't include a query component.
+        username: Available when the URI contains `User Information`_.
+        password: Available when the URI contains `User Information`_.
 
     .. _User Information: https://www.rfc-editor.org/rfc/rfc3986.html#section-3.2.1
 
@@ -30,8 +31,27 @@ class WebSocketURI:
     secure: bool
     host: str
     port: int
-    resource_name: str
-    user_info: Optional[Tuple[str, str]] = None
+    path: str
+    query: str
+    username: Optional[str]
+    password: Optional[str]
+
+    @property
+    def resource_name(self) -> str:
+        if self.path:
+            resource_name = self.path
+        else:
+            resource_name = "/"
+        if self.query:
+            resource_name += "?" + self.query
+        return resource_name
+
+    @property
+    def user_info(self) -> Optional[Tuple[str, str]]:
+        if self.username is None:
+            return None
+        assert self.password is not None
+        return (self.username, self.password)
 
 
 # All characters from the gen-delims and sub-delims sets in RFC 3987.
@@ -44,6 +64,9 @@ def parse_uri(uri: str) -> WebSocketURI:
 
     Args:
         uri: WebSocket URI.
+
+    Returns:
+        WebSocketURI: Parsed WebSocket URI.
 
     Raises:
         InvalidURI: if ``uri`` isn't a valid WebSocket URI.
@@ -60,16 +83,14 @@ def parse_uri(uri: str) -> WebSocketURI:
     secure = parsed.scheme == "wss"
     host = parsed.hostname
     port = parsed.port or (443 if secure else 80)
-    resource_name = parsed.path or "/"
-    if parsed.query:
-        resource_name += "?" + parsed.query
-    user_info = None
-    if parsed.username is not None:
-        # urllib.parse.urlparse accepts URLs with a username but without a
-        # password. This doesn't make sense for HTTP Basic Auth credentials.
-        if parsed.password is None:
-            raise exceptions.InvalidURI(uri, "username provided without password")
-        user_info = (parsed.username, parsed.password)
+    path = parsed.path
+    query = parsed.query
+    username = parsed.username
+    password = parsed.password
+    # urllib.parse.urlparse accepts URLs with a username but without a
+    # password. This doesn't make sense for HTTP Basic Auth credentials.
+    if username is not None and password is None:
+        raise exceptions.InvalidURI(uri, "username provided without password")
 
     try:
         uri.encode("ascii")
@@ -77,11 +98,11 @@ def parse_uri(uri: str) -> WebSocketURI:
         # Input contains non-ASCII characters.
         # It must be an IRI. Convert it to a URI.
         host = host.encode("idna").decode()
-        resource_name = urllib.parse.quote(resource_name, safe=DELIMS)
-        if user_info is not None:
-            user_info = (
-                urllib.parse.quote(user_info[0], safe=DELIMS),
-                urllib.parse.quote(user_info[1], safe=DELIMS),
-            )
+        path = urllib.parse.quote(path, safe=DELIMS)
+        query = urllib.parse.quote(query, safe=DELIMS)
+        if username is not None:
+            assert password is not None
+            username = urllib.parse.quote(username, safe=DELIMS)
+            password = urllib.parse.quote(password, safe=DELIMS)
 
-    return WebSocketURI(secure, host, port, resource_name, user_info)
+    return WebSocketURI(secure, host, port, path, query, username, password)

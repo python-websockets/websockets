077e6429df312f549c9ecf348ec63abfd249da28
Aymeric Augustin <aymeric.augustin@m4x.org>
2022-11-01 08:29:02 +0100

    Perform version check at compile time.
    This is a small performance optimization.

--- src/websockets/legacy/compatibility.py
+++ src/websockets/legacy/compatibility.py
@@ -5,9 +5,20 @@ import sys
 from typing import Any, Dict
 
 
-def loop_if_py_lt_38(loop: asyncio.AbstractEventLoop) -> Dict[str, Any]:
-    """
-    Helper for the removal of the loop argument in Python 3.10.
+if sys.version_info[:2] >= (3, 8):
 
-    """
-    return {"loop": loop} if sys.version_info[:2] < (3, 8) else {}
+    def loop_if_py_lt_38(loop: asyncio.AbstractEventLoop) -> Dict[str, Any]:
+        """
+        Helper for the removal of the loop argument in Python 3.10.
+
+        """
+        return {}
+
+else:  # pragma: no cover
+
+    def loop_if_py_lt_38(loop: asyncio.AbstractEventLoop) -> Dict[str, Any]:
+        """
+        Helper for the removal of the loop argument in Python 3.10.
+
+        """
+        return {"loop": loop}

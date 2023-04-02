47402cfe17cadfe1e32d3bb142693037364d4ce7
Aymeric Augustin <aymeric.augustin@m4x.org>
2023-04-02 07:44:51 +0200

    Backport typing.final for compatibility with Python 3.7.

--- src/websockets/legacy/async_timeout.py
+++ src/websockets/legacy/async_timeout.py
@@ -1,5 +1,5 @@
 # From https://github.com/aio-libs/async-timeout/blob/master/async_timeout/__init__.py
-# Licensed under the Apache License, Version 2.0.
+# Licensed under the Apache License (Apache-2.0)
 
 import asyncio
 import enum
@@ -9,11 +9,48 @@ from types import TracebackType
 from typing import Optional, Type
 
 
-if sys.version_info >= (3, 8):
+# From https://github.com/python/typing_extensions/blob/main/src/typing_extensions.py
+# Licensed under the Python Software Foundation License (PSF-2.0)
+
+if sys.version_info >= (3, 11):
     from typing import final
 else:
-    from typing_extensions import final
+    # @final exists in 3.8+, but we backport it for all versions
+    # before 3.11 to keep support for the __final__ attribute.
+    # See https://bugs.python.org/issue46342
+    def final(f):
+        """This decorator can be used to indicate to type checkers that
+        the decorated method cannot be overridden, and decorated class
+        cannot be subclassed. For example:
 
+            class Base:
+                @final
+                def done(self) -> None:
+                    ...
+            class Sub(Base):
+                def done(self) -> None:  # Error reported by type checker
+                    ...
+            @final
+            class Leaf:
+                ...
+            class Other(Leaf):  # Error reported by type checker
+                ...
+
+        There is no runtime checking of these properties. The decorator
+        sets the ``__final__`` attribute to ``True`` on the decorated object
+        to allow runtime introspection.
+        """
+        try:
+            f.__final__ = True
+        except (AttributeError, TypeError):
+            # Skip the attribute silently if it is not writable.
+            # AttributeError happens if the object has __slots__ or a
+            # read-only property, TypeError if it's a builtin class.
+            pass
+        return f
+
+
+# End https://github.com/aio-libs/async-timeout/blob/master/async_timeout/__init__.py
 
 __version__ = "4.0.2"
 
@@ -223,3 +260,6 @@ class Timeout:
         self._state = _State.TIMEOUT
         # drop the reference early
         self._timeout_handler = None
+
+
+# End https://github.com/aio-libs/async-timeout/blob/master/async_timeout/__init__.py

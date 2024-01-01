230d5052a33c0d940d926a1fc88909d39f57efd8
Aymeric Augustin <aymeric.augustin@fractalideas.com>
2024-01-01 15:41:54 +0100

    Add tests for abstract classes.
    This prevents Python 3.12 to complain that no test cases were run
    and to exit with code 5 (which breaks maxi_cov).

--- tests/extensions/test_base.py
+++ tests/extensions/test_base.py
@@ -1,4 +1,30 @@
+import unittest
+
 from websockets.extensions.base import *
+from websockets.frames import Frame, Opcode
 
 
-# Abstract classes don't provide any behavior to test.
+class ExtensionTests(unittest.TestCase):
+    def test_encode(self):
+        with self.assertRaises(NotImplementedError):
+            Extension().encode(Frame(Opcode.TEXT, b""))
+
+    def test_decode(self):
+        with self.assertRaises(NotImplementedError):
+            Extension().decode(Frame(Opcode.TEXT, b""))
+
+
+class ClientExtensionFactoryTests(unittest.TestCase):
+    def test_get_request_params(self):
+        with self.assertRaises(NotImplementedError):
+            ClientExtensionFactory().get_request_params()
+
+    def test_process_response_params(self):
+        with self.assertRaises(NotImplementedError):
+            ClientExtensionFactory().process_response_params([], [])
+
+
+class ServerExtensionFactoryTests(unittest.TestCase):
+    def test_process_request_params(self):
+        with self.assertRaises(NotImplementedError):
+            ServerExtensionFactory().process_request_params([], [])

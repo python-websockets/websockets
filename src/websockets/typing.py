python-websockets/websockets

commit 9038a62e7261af21109977407907038a1a0efc65

Make mypy 1.8.0 happy.

src/websockets/typing.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import http
 import logging
-from typing import List, NewType, Optional, Tuple, Union
+from typing import Any, List, NewType, Optional, Tuple, Union
 
 
 __all__ = [
@@ -28,7 +28,7 @@ Data = Union[str, bytes]
 """
 
 
-LoggerLike = Union[logging.Logger, logging.LoggerAdapter]
+LoggerLike = Union[logging.Logger, logging.LoggerAdapter[Any]]
 """Types accepted where a :class:`~logging.Logger` is expected."""
 
 

src/websockets/legacy/auth.py
@@ -159,7 +159,7 @@ def basic_auth_protocol_factory(
         if is_credentials(credentials):
             credentials_list = [cast(Credentials, credentials)]
         elif isinstance(credentials, Iterable):
-            credentials_list = list(credentials)
+            credentials_list = list(cast(Iterable[Credentials], credentials))
             if not all(is_credentials(item) for item in credentials_list):
                 raise TypeError(f"invalid credentials argument: {credentials}")
         else:

commit 3c6b1aab96adde1a4b0d3e8f1a93b7f2c7310af0

Restore compatibility with Python < 3.11.
Broken in 9038a62e.

src/websockets/typing.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import http
 import logging
+import typing
 from typing import Any, List, NewType, Optional, Tuple, Union
 
 
@@ -28,8 +29,12 @@ Data = Union[str, bytes]
 """
 
 
-LoggerLike = Union[logging.Logger, logging.LoggerAdapter[Any]]
-"""Types accepted where a :class:`~logging.Logger` is expected."""
+if typing.TYPE_CHECKING:
+    LoggerLike = Union[logging.Logger, logging.LoggerAdapter[Any]]
+    """Types accepted where a :class:`~logging.Logger` is expected."""
+else:  # remove this branch when dropping support for Python < 3.11
+    LoggerLike = Union[logging.Logger, logging.LoggerAdapter]
+    """Types accepted where a :class:`~logging.Logger` is expected."""
 
 
 StatusLike = Union[http.HTTPStatus, int]
python-websockets/websockets

commit 44ccee17c519ea1397ee28a8ac3a7d7685cd0b89

Drop Python 3.8.
It is EOL at the end of October.

src/websockets/sync/messages.py
@@ -3,7 +3,8 @@ from __future__ import annotations
 import codecs
 import queue
 import threading
-from typing import Iterator, cast
+from collections.abc import Iterator
+from typing import cast
 
 from ..exceptions import ConcurrencyError
 from ..frames import OP_BINARY, OP_CONT, OP_TEXT, Frame
@@ -150,8 +151,7 @@ class Assembler:
             chunks = self.chunks
             self.chunks = []
             self.chunks_queue = cast(
-                # Remove quotes around type when dropping Python < 3.9.
-                "queue.SimpleQueue[Data | None]",
+                queue.SimpleQueue[Data | None],
                 queue.SimpleQueue(),
             )
 

src/websockets/asyncio/messages.py
@@ -3,14 +3,8 @@ from __future__ import annotations
 import asyncio
 import codecs
 import collections
-from typing import (
-    Any,
-    AsyncIterator,
-    Callable,
-    Generic,
-    Iterable,
-    TypeVar,
-)
+from collections.abc import AsyncIterator, Iterable
+from typing import Any, Callable, Generic, TypeVar
 
 from ..exceptions import ConcurrencyError
 from ..frames import OP_BINARY, OP_CONT, OP_TEXT, Frame

.github/workflows/tests.yml
@@ -55,7 +55,6 @@ jobs:
     strategy:
       matrix:
         python:
-          - "3.8"
           - "3.9"
           - "3.10"
           - "3.11"

pyproject.toml
@@ -5,7 +5,7 @@ build-backend = "setuptools.build_meta"
 [project]
 name = "websockets"
 description = "An implementation of the WebSocket Protocol (RFC 6455 & 7692)"
-requires-python = ">=3.8"
+requires-python = ">=3.9"
 license = { text = "BSD-3-Clause" }
 authors = [
     { name = "Aymeric Augustin", email = "aymeric.augustin@m4x.org" },
@@ -19,7 +19,6 @@ classifiers = [
     "Operating System :: OS Independent",
     "Programming Language :: Python",
     "Programming Language :: Python :: 3",
-    "Programming Language :: Python :: 3.8",
     "Programming Language :: Python :: 3.9",
     "Programming Language :: Python :: 3.10",
     "Programming Language :: Python :: 3.11",

src/websockets/asyncio/client.py
@@ -4,8 +4,9 @@ import asyncio
 import logging
 import os
 import urllib.parse
+from collections.abc import AsyncIterator, Generator, Sequence
 from types import TracebackType
-from typing import Any, AsyncIterator, Callable, Generator, Sequence
+from typing import Any, Callable
 
 from ..client import ClientProtocol, backoff
 from ..datastructures import HeadersLike
@@ -492,7 +493,7 @@ class connect:
     # async for ... in connect(...):
 
     async def __aiter__(self) -> AsyncIterator[ClientConnection]:
-        delays: Generator[float, None, None] | None = None
+        delays: Generator[float] | None = None
         while True:
             try:
                 async with self as protocol:

src/websockets/asyncio/connection.py
@@ -8,16 +8,9 @@ import random
 import struct
 import sys
 import uuid
+from collections.abc import AsyncIterable, AsyncIterator, Awaitable, Iterable, Mapping
 from types import TracebackType
-from typing import (
-    Any,
-    AsyncIterable,
-    AsyncIterator,
-    Awaitable,
-    Iterable,
-    Mapping,
-    cast,
-)
+from typing import Any, cast
 
 from ..exceptions import (
     ConcurrencyError,

src/websockets/asyncio/server.py
@@ -6,17 +6,9 @@ import http
 import logging
 import socket
 import sys
+from collections.abc import Awaitable, Generator, Iterable, Sequence
 from types import TracebackType
-from typing import (
-    Any,
-    Awaitable,
-    Callable,
-    Generator,
-    Iterable,
-    Sequence,
-    Tuple,
-    cast,
-)
+from typing import Any, Callable, cast
 
 from ..exceptions import InvalidHeader
 from ..extensions.base import ServerExtensionFactory
@@ -905,9 +897,9 @@ def basic_auth(
 
     if credentials is not None:
         if is_credentials(credentials):
-            credentials_list = [cast(Tuple[str, str], credentials)]
+            credentials_list = [cast(tuple[str, str], credentials)]
         elif isinstance(credentials, Iterable):
-            credentials_list = list(cast(Iterable[Tuple[str, str]], credentials))
+            credentials_list = list(cast(Iterable[tuple[str, str]], credentials))
             if not all(is_credentials(item) for item in credentials_list):
                 raise TypeError(f"invalid credentials argument: {credentials}")
         else:

src/websockets/client.py
@@ -3,7 +3,8 @@ from __future__ import annotations
 import os
 import random
 import warnings
-from typing import Any, Generator, Sequence
+from collections.abc import Generator, Sequence
+from typing import Any
 
 from .datastructures import Headers, MultipleValuesError
 from .exceptions import (
@@ -313,7 +314,7 @@ class ClientProtocol(Protocol):
 
         self.writes.append(request.serialize())
 
-    def parse(self) -> Generator[None, None, None]:
+    def parse(self) -> Generator[None]:
         if self.state is CONNECTING:
             try:
                 response = yield from Response.parse(
@@ -374,7 +375,7 @@ def backoff(
     min_delay: float = BACKOFF_MIN_DELAY,
     max_delay: float = BACKOFF_MAX_DELAY,
     factor: float = BACKOFF_FACTOR,
-) -> Generator[float, None, None]:
+) -> Generator[float]:
     """
     Generate a series of backoff delays between reconnection attempts.
 

commit 4f4e64442e84763ca634f67ba0062c3fb8c985c6

Restore compatibility with Python 3.9.
It was broken in 44ccee17.

src/websockets/sync/messages.py
@@ -151,7 +151,8 @@ class Assembler:
             chunks = self.chunks
             self.chunks = []
             self.chunks_queue = cast(
-                queue.SimpleQueue[Data | None],
+                # Remove quotes around type when dropping Python < 3.10.
+                "queue.SimpleQueue[Data | None]",
                 queue.SimpleQueue(),
             )
 
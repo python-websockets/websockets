f7a3dbffdf4fe360cc7d1f133df97f42daa86ee9
Aymeric Augustin <aymeric.augustin@fractalideas.com>
2026-09-12 13:11:04 +0200

    Explain why set_insecure is actually safe.

--- src/websockets/legacy/http.py
+++ src/websockets/legacy/http.py
@@ -175,6 +175,8 @@ async def read_headers(stream: asyncio.StreamReader) -> Headers:
 
         name = raw_name.decode("ascii")  # guaranteed to be ASCII at this point
         value = raw_value.decode("ascii", "surrogateescape")
+
+        # Since we just validated raw_value, we don't need to revalidate it.
         headers.set_insecure(name, value)
 
     else:
